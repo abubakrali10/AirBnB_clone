ad75179da7879af443f545eb2a219cbd9c236e09
abubakrali10 <bakrali175@yahoo.com>
2023-07-20 18:26:20 +0300

    implemented save method to update instance of BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     Base class for models.
@@ -42,4 +43,10 @@ class BaseModel:
                     setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """
+        Updates the updated_at attribute with the current datetime.
+        """
         self.updated_at = datetime.now()
